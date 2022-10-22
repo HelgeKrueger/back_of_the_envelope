2390b048789ac3fac753c154c8b23f6b5bb0a5a2
Helge <helge.krueger@gmail.com>
2022-10-22 17:11:06 +0200

    Actually use a model to make a prediction

--- webapp/src/components/United_Kingdom_2022_10_21.js
+++ webapp/src/components/United_Kingdom_2022_10_21.js
@@ -7,8 +7,40 @@ import {
 import { Container } from "@mui/system";
 import PercentageBarChart from "./PercentageBarChart";
 import Sources from "./Sources";
+import SyntaxHighlighter from "react-syntax-highlighter";
 
 const Predicition = () => {
+  const code1 = `mp_votes_base = {
+    'johnson': 52,
+    'sunak': 111,
+    'mordaunt': 23,
+    'other': 0
+} # From G3
+total_mp_tories = 357
+
+left_over_mp = total_mp_tories - sum(mp_votes_base.values())
+print(f"Remaining MPS {left_over_mp}")
+
+def random_vote():
+    votes = dict(mp_votes_base)
+    for _ in range(left_over_mp):
+        possible_choices = [key for key, value in votes.items()]
+        vote  = random.choice(possible_choices)
+        votes[vote] += 1
+    return votes
+
+chances = {name: 0 for name in mp_votes_base.keys()}
+
+for _ in range(1000):
+    vote = random_vote()
+    choices = [key for key, value in vote.items() if value >= 100]
+    next_pm = random.choice(choices)
+    chances[next_pm] += 1
+
+chances
+
+{'johnson': 101, 'sunak': 899, 'mordaunt': 0, 'other': 0}
+`;
   return (
     <Accordion defaultExpanded>
       <AccordionSummary>
@@ -23,6 +55,19 @@ const Predicition = () => {
             <b>Note: </b> Just getting started; Still didn't get around to build
             a proper model; just guesstimating probabilities
           </Typography>
+        </Container>
+        <Typography variant="h5">Saturday [G3] + simple model</Typography>
+        <Container>
+          <PercentageBarChart
+            data={[
+              { name: "Rishi Sunak", percentage: 90 },
+              { name: "Penny Mordaunt", percentage: 0 },
+              { name: "Boris Johnson", percentage: 10 },
+            ]}
+            title="Chance to be next prime minister"
+          />
+          Using G3 and
+          <SyntaxHighlighter language="python">{code1}</SyntaxHighlighter>
         </Container>
         <Typography variant="h5">Saturday [G2]</Typography>
         <PercentageBarChart
@@ -62,6 +107,12 @@ const Predicition = () => {
             },
             {
               name: "G2",
+              title:
+                "The Guardian: Johnson, Mordaunt or Sunak: who is backing whom as next Tory leader",
+              url: "https://www.theguardian.com/politics/2022/oct/21/boris-johnson-penny-mordaunt-rishi-sunak-who-backing-next-tory-leader",
+            },
+            {
+              name: "G3",
               title:
                 "The Guardian: Johnson, Mordaunt or Sunak: who is backing whom as next Tory leader",
               url: "https://www.theguardian.com/politics/2022/oct/21/boris-johnson-penny-mordaunt-rishi-sunak-who-backing-next-tory-leader",
