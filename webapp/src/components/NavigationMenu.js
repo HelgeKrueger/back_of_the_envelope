9334322adbf150547bedde659bdecce1f3db37c1
Helge <helge.krueger@gmail.com>
2022-11-14 18:35:06 +0100

    Small updates

--- webapp/src/components/NavigationMenu.js
+++ webapp/src/components/NavigationMenu.js
@@ -3,16 +3,32 @@ import {
   List,
   ListItem,
   ListItemButton,
+  ListItemIcon,
   ListItemText,
 } from "@mui/material";
+
+import HomeIcon from "@mui/icons-material/Home";
+
 import Germany from "../pages/germany";
 import UnitedKingdom from "../pages/unitedkingdom";
-
+import Welcome from "../pages/welcome";
 const NavigationMenu = (props) => {
   const { setPage } = props;
   return (
     <Drawer variant="permanent" sx={{ backgroundColor: "lightgray" }}>
       <List sx={{ width: "300px" }}>
+        <ListItem>
+          <ListItemButton
+            onClick={() => {
+              setPage(<Welcome setPage={setPage} />);
+            }}
+          >
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Home"} />
+          </ListItemButton>
+        </ListItem>{" "}
         <ListItem>
           <ListItemButton
             onClick={() => {
